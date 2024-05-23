536ab6182b428e9404231e5c09c3b756f259c27b
hmapedro <78369129+hmapedro@users.noreply.github.com>
2024-05-23 16:51:35 +0100

    Alteração de consentimentos com dados SFRAA

--- SportNow Maui New/Views/CompleteRegistration/ConsentPageCS.cs
+++ SportNow Maui New/Views/CompleteRegistration/ConsentPageCS.cs
@@ -56,7 +56,7 @@ namespace SportNow.Views.CompleteRegistration
             absoluteLayout.SetLayoutBounds(scrollView, new Rect(10 * App.screenWidthAdapter, 10 * App.screenHeightAdapter, App.screenWidth - 20 * App.screenWidthAdapter, App.screenHeight - 120 * App.screenHeightAdapter));
 
 
-			Microsoft.Maui.Controls.Grid gridConsent = new Microsoft.Maui.Controls.Grid { Padding = 0, HorizontalOptions = LayoutOptions.FillAndExpand };
+			Microsoft.Maui.Controls.Grid gridConsent = new Microsoft.Maui.Controls.Grid { Padding = 0, HorizontalOptions = LayoutOptions.FillAndExpand, RowSpacing = 10 * App.screenHeightAdapter };
             scrollView.Content = gridConsent;
             gridConsent.RowDefinitions.Add(new RowDefinition { Height = GridLength.Star });
             gridConsent.RowDefinitions.Add(new RowDefinition { Height = 100 * App.screenHeightAdapter });
@@ -66,16 +66,16 @@ namespace SportNow.Views.CompleteRegistration
             gridConsent.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star }); //GridLength.Auto 
 
 			Label labelRegulamentoInterno = new Label { FontFamily = "futuracondensedmedium", BackgroundColor = Colors.Transparent, VerticalTextAlignment = TextAlignment.Start, HorizontalTextAlignment = TextAlignment.Start, FontSize = App.consentFontSize, TextColor = App.normalTextColor, LineBreakMode = LineBreakMode.WordWrap };
-			labelRegulamentoInterno.Text = "O NKS na execução da atividade laboral, acede e lida, quando necessário, com os dados pessoais dos nossos atletas, sócios, parceiros e demais atores que intervém para o desenrolar na nossa atividade associativa. Esta comunicação pretende informar da forma como o NKS processa esses dados pessoais e qual a finalidade do tratamento das mesmas, assim como informar dos direitos que assistem aos titulares dos dados e da forma como os mesmos poderão exercer esses mesmos direitos.\n\nEntidade responsável pelo tratamento:\nNKS - Núcleo Karate de Sangalhos – NIF: 509 923 194\nCom morada na Rua Ivo Neves, n.º405, 3780-524 Sangalhos\nE-mail: nks@karatesangalhos.com\n\nTratamos os dados pessoais para as seguintes finalidades:\nPrestação de serviços relacionados com a nossa atividade associativa;\nCumprimento de obrigações legais;\nEfeitos de marketing e redes sociais.\n\nO NKS poderá ter de comunicar os dados pessoais recolhidos a terceiros que estejam envolvidos na prestação dos serviços por si contratados, assim como a autoridades judiciais, fiscais e regulatórias, com a finalidade do cumprimento de obrigações legais. Os dados pessoais recolhidos serão exclusivamente tratados para as finalidades acima indicadas pelo NKS e serão conservados somente durante o período necessário à concretização das finalidades que motivaram a sua recolha.";
+			labelRegulamentoInterno.Text = "A SFRAA na execução da atividade laboral, acede e lida, quando necessário, com os dados pessoais dos nossos atletas, sócios, parceiros e demais atores que intervém para o desenrolar na nossa atividade associativa. Esta comunicação pretende informar da forma como a SFRAA processa esses dados pessoais e qual a finalidade do tratamento das mesmas, assim como informar dos direitos que assistem aos titulares dos dados e da forma como os mesmos poderão exercer esses mesmos direitos.\n\nEntidade responsável pelo tratamento:\nEntidade: SFRAA – Sociedade Filarmónica de Apoio Social e Recreio Artístico da Amadora\nNIF: 501 412 506\nMorada: Rua Elias Garcia, n.º142, 2700-331 Amadora\nE-mail: geral@sfraa.pt\n\nTratamos os dados pessoais para as seguintes finalidades:\nPrestação de serviços relacionados com a nossa atividade associativa;\nCumprimento de obrigações legais;\nEfeitos de marketing e redes sociais.\n\nA SFRAA poderá ter de comunicar os dados pessoais recolhidos a terceiros que estejam envolvidos na prestação dos serviços por si contratados, assim como a autoridades judiciais, fiscais e regulatórias, com a finalidade do cumprimento de obrigações legais. Os dados pessoais recolhidos serão exclusivamente tratados para as finalidades acima indicadas pela SFRAA e serão conservados somente durante o período necessário à concretização das finalidades que motivaram a sua recolha.";
 
-            Label rgpdLabel = new Label { FontFamily = "futuracondensedmedium", Text = "Consulta aqui a Política de Tratamento de Dados do NK Sangalhos", VerticalTextAlignment = TextAlignment.Start, HorizontalTextAlignment = TextAlignment.Center, FontSize = 13 * App.screenWidthAdapter, TextColor = App.topColor, LineBreakMode = LineBreakMode.NoWrap };
+            Label rgpdLabel = new Label { FontFamily = "futuracondensedmedium", Text = "Consulta aqui a Política de Tratamento de Dados da SFRAA", VerticalTextAlignment = TextAlignment.Start, HorizontalTextAlignment = TextAlignment.Center, FontSize = 13 * App.screenWidthAdapter, TextColor = App.topColor, LineBreakMode = LineBreakMode.NoWrap };
 
             var rgpdLabel_tap = new TapGestureRecognizer();
             rgpdLabel_tap.Tapped += async (s, e) =>
             {
                 try
                 {
-                    await Browser.OpenAsync("https://karatesangalhos.pt/rgpd", BrowserLaunchMode.SystemPreferred);
+                    await Browser.OpenAsync("http://www.sfraa.pt/http-www-sfraa-pt-wp-content-uploads-politica-de-privacidade-pdf/", BrowserLaunchMode.SystemPreferred);
                 }
                 catch (Exception ex)
                 {
